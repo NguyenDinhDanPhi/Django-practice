65a7c872ef03873cd2a8fb6d4bb56474fa265f1b
phi <phindd@fpt.com>
2025-11-19 17:43:57 +0700

    chap 12: database queries basic

--- firstDemo/devsearch/projects/views.py
+++ firstDemo/devsearch/projects/views.py
@@ -1,38 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
-projectsList = [
-    {
-        'id':'1',
-        'title':"Ecommerce Website",
-        'description':'Fully functional ecommerce website'
-    },
-    {
-        'id':'2',
-        'title':"Portfolio Website",
-        'description':'This was a project where I built out my portfol.'
-    },
-    {
-        'id':'3',
-        'title':"Social Network",
-        'description':'Awesome open source project I am still working'
-    },
-]
 
 
 def homePage(request):
     return HttpResponse('Hello world')
 
 def projects(request):
-    page = 'projects'
-    number = 10
-    context = {'page': page, 'number': number, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = { 'projects': projects}
 
     return render(request, 'projects/projects.html',context)
 
 def project(request,pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request,'projects/single-project.html', {'project': projectObj})
