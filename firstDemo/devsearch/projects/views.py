bd72e32a0c08894441e7f460ab4c4d5eb04d744a
phi <phindd@fpt.com>
2025-11-20 15:25:27 +0700

    chap 13 final: base form

--- firstDemo/devsearch/projects/views.py
+++ firstDemo/devsearch/projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
+from .forms import ProjectForm
 
 
 def homePage(request):
@@ -16,3 +16,8 @@ def projects(request):
 def project(request,pk):
     projectObj = Project.objects.get(id=pk)
     return render(request,'projects/single-project.html', {'project': projectObj})
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request,'projects/project_form.html', context)
