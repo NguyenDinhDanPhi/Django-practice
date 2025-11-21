677e2d85e381b99603f76aae7190593b04d8af72
phi <phindd@fpt.com>
2025-11-21 17:29:18 +0700

    final 15: static file

--- firstDemo/devsearch/projects/views.py
+++ firstDemo/devsearch/projects/views.py
@@ -20,7 +20,7 @@ def project(request,pk):
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
-        form = ProjectForm(request.POST)
+        form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -31,7 +31,7 @@ def updateProject(request,pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
-        form = ProjectForm(request.POST, instance=project)
+        form = ProjectForm(request.POST,request.FILES ,instance=project)
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -45,4 +45,3 @@ def deleteProject(request, pk):
         project.delete()
         return redirect('projects')
     return render(request,'projects/delete_template.html',context )
-#this is test diff indicators 
