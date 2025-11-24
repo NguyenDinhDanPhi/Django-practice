865ce9f6c8bbbd65de352b251879ed5c99152d87
phi <phindd@fpt.com>
2025-11-24 16:03:45 +0700

    fiinel chap 16: theme app

--- firstDemo/devsearch/projects/forms.py
+++ firstDemo/devsearch/projects/forms.py
@@ -1,7 +1,15 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'feature_image','description','demo_link', 'source_link','source_link','tags']
+
+        widgets = {'tags': forms.CheckboxSelectMultiple(),}
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm,self).__init__()
+        for k,v in self.fields.items():
+            v.widget.attrs.update({'class': 'input'})
+       
