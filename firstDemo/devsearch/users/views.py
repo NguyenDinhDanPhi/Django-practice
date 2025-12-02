02f98206166a7a6efe1e3bf976784979fe2d4d31
phi <phindd@fpt.com>
2025-12-02 15:18:32 +0700

    chap18: final

--- firstDemo/devsearch/users/views.py
+++ firstDemo/devsearch/users/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
+
+def userProfile(request,pk):
+    profile = Profile.objects.get(id=pk)
+    topSkill = profile.skill_set.exclude(description__exact="")
+    otherSkill = profile.skill_set.filter(description="")
+    context = {'profile':profile,'topSkill':topSkill,'otherSkill': otherSkill}
+    return render(request, 'users/user-profile.html',context)
