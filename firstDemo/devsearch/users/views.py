4fb9b0d159b4b1cf41ddeadc7bca5d3cd0ccdd13
phi <phindd@fpt.com>
2025-12-03 14:00:36 +0700

    chapter 20: login lougout

--- firstDemo/devsearch/users/views.py
+++ firstDemo/devsearch/users/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Profile
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def profiles(request):
     profiles = Profile.objects.all()
@@ -12,3 +15,28 @@ def userProfile(request,pk):
     otherSkill = profile.skill_set.filter(description="")
     context = {'profile':profile,'topSkill':topSkill,'otherSkill': otherSkill}
     return render(request, 'users/user-profile.html',context)
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get()
+        except:
+            messages.error(request, 'Username does not exist')
+            user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or pass not match')
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User was logout')
+    return redirect('login')
